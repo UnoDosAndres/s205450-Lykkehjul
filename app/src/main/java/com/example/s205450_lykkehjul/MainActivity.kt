2fef7be73641f70341f48cf8b84ab6353495aabd
UnoDosAndres <s205450@student.dtu.dk>
2021-12-03 22:19:03 +0100

    Hangman delen i næsten færdig (mangler win/lose)

--- app/src/main/java/com/example/s205450_lykkehjul/MainActivity.kt
+++ app/src/main/java/com/example/s205450_lykkehjul/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.s205450_lykkehjul
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.animation.*
 import android.widget.Toast
 import com.example.s205450_lykkehjul.databinding.ActivityMainBinding
@@ -21,12 +24,11 @@ class MainActivity : AppCompatActivity() {
     private var isSpinning = false
     private var degreePoint = 0
     lateinit var wordToBeGuessed: String
-    lateinit var wordDisplayed: String
+    private var wordDisplayed: String = ""
     lateinit var charArray: CharArray
     private var listOfWords: ArrayList<String> = arrayListOf()
     lateinit var lettersTried: String
-    private val winMessage= "You Won!"
-    private val loseMessage= "You Lost!"
+    private var lives = 5
 
     /*For the part about reading of a txt and general code about the 'guess a word part of the game
         i have taken inspiration from this playlist on youtube
@@ -38,13 +40,14 @@ class MainActivity : AppCompatActivity() {
     https://www.youtube.com/watch?v=5O2Uox-TR00
      */
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.livesLeft.text = "Lives left: $lives"
 
-        val myInputStream:InputStream
-        myInputStream = assets.open("database_of_words.txt")
+        val myInputStream:InputStream = assets.open("database_of_words.txt")
         val myScanner = Scanner(myInputStream)
         while (myScanner.hasNext()){
             val wordHolder = myScanner.next()
@@ -56,6 +59,22 @@ class MainActivity : AppCompatActivity() {
         getSectorDegrees()
         startGame()
 
+        binding.letterGuess.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                if (s.isNotEmpty()) {
+                    checkLetter(s[0])
+                }
+            }
+        })
+
         binding.spinKnap.setOnClickListener(){
             if (!isSpinning){
                 isSpinning = true
@@ -63,13 +82,74 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-
+    }
+    //checks if the letter is correct and shows it
+    //also checks if we have a winner
+    @SuppressLint("SetTextI18n")
+    private fun checkLetter(letter: Char){
+        if (wordToBeGuessed.indexOf(letter) >= 0 && wordDisplayed.indexOf(letter) < 0) {
+            showLetterInWord(letter)
+            displayWordInApp()
+            if (!wordDisplayed.contains("_") && lives > 0) {
+                //here the win fragment should show
+            }
+        }
+        else {
+            lives = lives-1
+            binding.livesLeft.text = "Lives left: $lives"
+            if (lives <= 0) {
+                //Here the lose fragment should show
+            }
+            if (lettersTried.indexOf(letter) < 0) {
+                lettersTried += "$letter, "
+                binding.lettersUsed.text = "Letters tried: $lettersTried"
+            }
+        }
 
     }
+
+    //starts the game
     private fun startGame(){
+        listOfWords.shuffle()
+        wordToBeGuessed = listOfWords[0]
+        listOfWords.remove(listOfWords[0])
+        charArray = wordToBeGuessed.toCharArray()
+
+        for (i in charArray.indices) {
+            charArray[i] = '_'
+        }
+        wordDisplayed.toCharArray(charArray)
+
+        displayWordInApp()
+
+        binding.letterGuess.setText("")
+
+        lettersTried = " "
 
     }
 
+    //reveals the chosen letter
+    private fun showLetterInWord(letter:Char) {
+        var letterIndex = wordToBeGuessed.indexOf(letter)
+        while (letterIndex >= 0) {
+            charArray[letterIndex] = wordToBeGuessed.get(letterIndex)
+            //For at sikre jeg kan finde flere af det samme bogstav
+            letterIndex = wordToBeGuessed.indexOf(letter, letterIndex+1)
+        }
+
+        wordDisplayed.toCharArray(charArray)
+    }
+
+    //updates the view so we see the newly revealed letter
+    private fun displayWordInApp(){
+        var formattedString = ""
+        for (i in charArray) {
+            formattedString +=  "$i "
+        }
+        binding.word.text = formattedString
+    }
+
+    // Spins the wheel
     // i ended up being unable to get it to show the right amount of points from the wheel, but it does show some
     private fun spin() {
         degree = random.nextInt(sectors.size-1)
